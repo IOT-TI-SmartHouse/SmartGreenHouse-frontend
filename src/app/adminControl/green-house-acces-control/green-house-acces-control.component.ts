61090136b3a96b61af387966f73e57d2c7a72669
Berend Vet <kantoor+berend@asamco.com>
2018-05-24 13:09:00 +0200

    filter function

--- src/app/adminControl/green-house-acces-control/green-house-acces-control.component.ts
+++ src/app/adminControl/green-house-acces-control/green-house-acces-control.component.ts
@@ -48,6 +48,8 @@ export class GreenHouseAccesControlComponent implements OnInit {
     this.greenhouseService.getAll(user._id).subscribe( res => {
       console.log(res);
       this.greenhousesForUser = (res as any).users;
+      // filter the greenhouses list to only contain greenhouses that the user does not already have access to
+      this.greenhouses = this.filterList(this.greenhouses, this.greenhousesForUser);
     });
     console.log(this.greenhousesForUser);
   }
@@ -56,7 +58,23 @@ export class GreenHouseAccesControlComponent implements OnInit {
     this.selectedGreenhouse = greenhouse;
   }
 
-
+  /**
+   * funtion that filters a list with all elements from a second list
+   * @param fullList list to filter
+   * @param secondList list that contains the elements to filter out
+   */
+  filterList(fullList: any[], secondList: any[]): any[] {
+    let found;
+    return fullList.filter( firstElement => {
+      found = false;
+      secondList.forEach( secondElement => {
+          if (firstElement._id === secondElement._id) {
+              found = true;
+          }
+      });
+      return found;
+    });
+  }
 
   // @ViewChild('instance') instance: NgbTypeahead;
   // focus$ = new Subject<string>();
