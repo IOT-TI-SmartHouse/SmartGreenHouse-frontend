392bbd625450da1d8cf17fc0bc4dc0075e053a89
Christiaan Paans <christiaanpaans@outlook.com>
2018-05-31 12:50:34 +0200

    dropdown in navbar, bootstrap rounded on login page

--- src/app/menu/menubar/menubar.component.ts
+++ src/app/menu/menubar/menubar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppModule } from '../../app.module';
 import { Event as RouterEvent, NavigationCancel, NavigationEnd, NavigationError, Router, Routes} from '@angular/router';
 import { LoginService } from '../../login/services/login.service';
@@ -9,12 +9,38 @@ import { LoginService } from '../../login/services/login.service';
   styleUrls: ['./menubar.component.css']
 })
 
-export class MenuBarComponent {
-    title = 'Smart Greenhouse Application';
+export class MenuBarComponent implements OnInit {
+  public selectedTab: SelectedTab = SelectedTab.USER;
+  title = 'Smart Greenhouse Application';
 
-    constructor(private router: Router, private loginService: LoginService) { }
+  constructor(private router: Router, private loginService: LoginService) { }
+  logout() {
+    this.loginService.logout();
+  }
 
-    logout() {
-        this.loginService.logout();
-    }
+  ngOnInit() {
+  }
+
+  navigateUser() {
+    this.selectedTab = SelectedTab.USER;
+  }
+
+  navigateGreenhouse() {
+    this.selectedTab = SelectedTab.GREENHOUSE;
+  }
+
+  navigateGreenhouseAccess() {
+    this.selectedTab = SelectedTab.GREENHOUSEACCESS;
+  }
+
+  navigateGreenhouseDepartment() {
+    this.selectedTab = SelectedTab.DEPARTMENT;
+  }
+}
+
+enum SelectedTab {
+  USER = 0,
+  GREENHOUSE = 1,
+  GREENHOUSEACCESS = 2,
+  DEPARTMENT = 3
 }
